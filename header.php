6ec5a23bb42b7324294b2531c7d99a5b8bf77abe
themetalbacon <ageisalie@gmail.com>
2017-04-22 21:00:26 -0400

    Moved over humescore code align header

--- header.php
+++ header.php
@@ -13,18 +13,34 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> style="background-image:url(http://i.imgur.com/UKQJ3xV.png); background-attachment:fixed" >
+
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'candyland' ); ?></a>
+
+	<?php if ( is_front_page()) : ?>
+	<div style="position: relative;" class="header-container">
+		<figure class="header-image">
+			<img src="http://svgshare.com/i/1J2.svg" alt="">
+		</figure>
+		<div style="position: absolute;">
+			<?php echo file_get_contents("./wp-content/themes/candyland/images/logosvg.svg"); ?>
+		</div>
+	</div>
+	<?php endif; // End header image check. ?>
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+
+			<?php the_custom_logo() ; ?>
+
+			<div class=site-branding__text>
+
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -38,11 +54,12 @@
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
 			endif; ?>
+		</div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'candyland' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'humescores' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
