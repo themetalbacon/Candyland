cbf1440ec90a20f94a328db9b583e81a733d4e11
themetalbacon <ageisalie@gmail.com>
2017-04-29 11:48:39 -0400

    display chat box even with no widgets, nav styling

--- sidebar.php
+++ sidebar.php
@@ -7,16 +7,19 @@
  * @package Yummy_Gummy_Opening
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+/*if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
-}
+}*/
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary">
 	<section id="chatbox" class="widget chatbox" style="height:400px;">
-		<script id="cid0020000153956491537" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"testst11","arch":"js","styles":{"a":"bf5c92","b":100,"c":"000000","d":"000000","k":"bf5c92","l":"bf5c92","m":"bf5c92","q":"bf5c92","r":100,"fwtickm":1}}</script>
+		<script id="cid0020000154341072412" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"ygscans","arch":"js","styles":{"a":"BF5C92","b":100,"c":"000000","d":"000000","k":"BF5C92","l":"BF5C92","m":"BF5C92","p":"10","q":"BF5C92","r":100,"fwtickm":1}}</script>
 	</section>
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php if (  is_active_sidebar( 'sidebar-1' ) ) {
+			 dynamic_sidebar( 'sidebar-1' );
+		 }
+	?>
 </aside>
 
 <!-- <aside id="secondary" class="widget-area" role="complementary">
